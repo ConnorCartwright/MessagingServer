a48e239cf03dadbf6b2bbd8a37f6c1f44ef85132
Connor0728 <connorcart@gmail.com>
2016-03-03 23:53:55 +0000

    Added to do client.js file

---
+++ www/client.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  // create function for user joined
+
+  // create function for user leaving
+
+  // create function for user typing
+
+  // create function for user stopped typing
+
+  // create function for new message
+
+});
